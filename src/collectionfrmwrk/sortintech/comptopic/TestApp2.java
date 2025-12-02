db0ba5a18b468780dc6fddfe7b8384bd90c88bdb
rahul kirpekar <rahul.kirpekar@gmail.com>
2025-12-02 18:53:06 +0530

    sorting by comparator

---
+++ src/collectionfrmwrk/sortintech/comptopic/TestApp2.java
@@ -0,0 +1,49 @@
+package collectionfrmwrk.sortintech.comptopic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class TestApp2 
+{
+	public static void main(String[] args) 
+	{
+		Student s1 = new Student(1, "Rahul", 12);
+		Student s2 = new Student(2, "Ankur", 8);
+		Student s3 = new Student(3, "Sagar", 4);
+		Student s4 = new Student(4, "Krunal", 9);
+		Student s5 = new Student(5, "Dev", 10);
+		
+		ArrayList<Student> list = new ArrayList<Student>();
+		list.add(s1);
+		list.add(s2);
+		list.add(s3);
+		list.add(s4);
+		list.add(s5);
+		
+		for(int i = 0 ; i <  list.size();i++) 
+		{
+			Student s= list.get(i);
+			s.dispData();
+		}
+		System.out.println("--------------------------------------");
+		System.out.println("StdWise Sorting : ");
+
+		Collections.sort(list, new StdWiseStudentComparator());
+		
+		for(int i = 0 ; i <  list.size();i++) 
+		{
+			Student s= list.get(i);
+			s.dispData();
+		}
+		System.out.println("--------------------------------------");
+		System.out.println("NameWise Sorting : ");
+
+		Collections.sort(list, new NameWiseStudentComparator());
+		
+		for(int i = 0 ; i <  list.size();i++) 
+		{
+			Student s= list.get(i);
+			s.dispData();
+		}
+	}
+}
